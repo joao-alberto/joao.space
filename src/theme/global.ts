52077c64bce903b8d9c2b33f0298b7b26436bf60
João Alberto <joaoalberto.contato@gmail.com>
2021-04-09 20:26:33 -0300

    Redesigning

---
+++ src/theme/global.ts
@@ -0,0 +1,10 @@
+import { global } from "@stitches/react";
+
+const globalStyles = global({
+  body: {
+    margin: 0,
+    overflow: "auto",
+  },
+});
+
+export default globalStyles;
--- src/utils/strip-frontmatter.js
+++
@@ -1,7 +0,0 @@
-import fm from "front-matter"
-
-export function stripFrontmatter(markdown) {
-  const data = fm(markdown)
-
-  return data.body
-}
